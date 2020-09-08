b4fcdbfd9ad41502987c948fe46109461cfde1ee
Sergio <xabarov-05@mail.ru>
2020-09-08 21:02:12 +0300

    Game name fix

---
+++ Assets/scripts/buttons.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class buttons : MonoBehaviour
+{
+    void OnMouseDown () {
+        transform.localScale = new Vector3 (0.02f, 0.02f, 0.02f);
+    }
+
+    void OnMouseUp ()
+    {
+        transform.localScale = new Vector3 (0.022f, 0.022f, 0.022f);
+    }
+}
