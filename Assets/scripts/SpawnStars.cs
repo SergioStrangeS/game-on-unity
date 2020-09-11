d451dbe1a54b091da84bab76a75e92997620d36d
Little language <47820811+Sereoj@users.noreply.github.com>
2020-09-11 19:37:51 +0300

    Add the classes "Spawn" & "Animation"  stars
    Добавление классов, пояления и анимации звезд.

---
+++ Assets/scripts/SpawnStars.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnStars : MonoBehaviour
+{
+    private GameObject Star;
+    private Camera cam;
+
+    void Start()
+    {
+        StartCoroutine("SpawnRandom");
+        cam = Camera.main;
+    }
+
+    private IEnumerable SpawnRandom()
+    {
+        while(true)
+        {
+            Vector3 screenPosition = cam.ScreenToWorldPoint(
+                new Vector3(
+                Random.Range(0, Screen.width),
+                Random.Range(0, Screen.height),
+                cam.farClipPlane / 2f));
+            Instantiate(Star, screenPosition, Quaternion.identity);
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
---
+++ Assets/scripts/StarAnimation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarAnimation : MonoBehaviour
+{
+    private SpriteRenderer Star;
+
+    void Start()
+    {
+        Star = GetComponent<SpriteRenderer>();
+        Destroy(gameObject, 5f);
+    }
+    void Update()
+    {
+        Star.color = new Color(Star.color.r, Star.color.g, Star.color.b, Mathf.PingPong(Time.time / 2.5f, 1.0f));
+    }
+}
